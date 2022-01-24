376180f9d8d5a171aa4aa6cf26330482c35350fe
Samantha Klonaris <samanthaklonaris@Samanthas-MacBook-Air-2.local>
2022-01-23 19:58:57 -0500

    first draft

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function setup() {
+  	// open/close faq item
+	$( ".faq-item" ).click(function(e) {
+		$(this).find('.faq-body').toggleClass('hidden')
+		$(this).find('.arrow-img').toggleClass('up')
+	});
+
+
+	// move hero image
+	var HERO_2_OFFSET = -300
+	$(window).on("load resize scroll", function() {
+	  $("#hero-pane").each(function() {
+	    var windowTop = $(window).scrollTop();
+	    var elementTop = $(this).offset().top;
+	    var bottomPosition = windowTop - elementTop + HERO_2_OFFSET;
+	    if (bottomPosition <= 100) {
+	    	$(this).find("#hero-2").css({ bottom: bottomPosition });
+	    }
+	  });
+	});
+}
+
+
+window.onload = setup
