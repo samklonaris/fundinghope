58be6578bbf58b211ccbf29fc4e797e384abb430
Samantha Klonaris <srklonaris@gmail.com>
2022-01-30 17:22:55 -0500

    mobile responsive

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,27 @@ function setup() {
 
 
 	// move hero image
+	var HERO_1_OFFSET = 50
 	var HERO_2_OFFSET = -300
 	$(window).on("load resize scroll", function() {
 	  $("#hero-pane").each(function() {
+	  	// No scroll on mobile
+  		if (window.innerWidth < 1190) {
+  			return
+  		}
+
 	    var windowTop = $(window).scrollTop();
 	    var elementTop = $(this).offset().top;
 	    var bottomPosition = windowTop - elementTop + HERO_2_OFFSET;
 	    if (bottomPosition <= 400) {
 	    	$(this).find("#hero-2").css({ bottom: bottomPosition });
 	    }
+
+		var topPosition = HERO_1_OFFSET - ((windowTop - elementTop) / 4);
+		if (800 <= topPosition <= 10000000) {
+	    	$(this).find("#hero-1").css({ top: topPosition });
+	    }
+
 	  });
 	});
 }
